b6beebbf3ff61f5f5aca56ea3f5828f1956dfacb
Ubuntu <ubuntu@ip-172-31-81-64.ec2.internal>
2024-09-04 10:44:10 +0000

    push

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
     res.send('Hello, World!');
+=======
+    res.send('Welocome To CICD Pipeline!');
+>>>>>>> 2d73bed (code)
 });
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2d73bed (code)
